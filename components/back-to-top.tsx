aafb0b134ddda0bf3d9f7388c015aa6da9f35cdd
mdazkaar <azkar@jcchaudhry.com>
2026-08-06 10:49:46 +0530

    refactor: revert BackToTop component to left-aligned position

--- components/back-to-top.tsx
+++ components/back-to-top.tsx
@@ -37,7 +37,7 @@ export default function BackToTop() {
           animate={{ opacity: 1, scale: 1 }}
           exit={{ opacity: 0, scale: 0.8 }}
           transition={{ duration: 0.2 }}
-          className="fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50"
+          className="fixed bottom-6 left-6 z-50"
         >
           <Button
             onClick={scrollToTop}
